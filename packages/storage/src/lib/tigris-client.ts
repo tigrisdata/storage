55c8d229b443ca0ab02cae2fcbe02a3aa5800d80
Abdullah Ibrahim <aibrahim@tigrisdata.com>
2025-10-23 15:40:23 +0200

    feat: add extra options for createBucket input

--- packages/storage/src/lib/tigris-client.ts
+++ packages/storage/src/lib/tigris-client.ts
@@ -1,8 +1,13 @@
 import { S3Client } from '@aws-sdk/client-s3';
+import type { HttpRequest } from '@aws-sdk/types';
 import { config, missingConfigError } from './config';
 import type { TigrisStorageConfig, TigrisStorageResponse } from './types';
 
 export enum TigrisHeaders {
+  NAMESPACE = 'X-Tigris-Namespace',
+  STORAGE_CLASS = 'X-Amz-Storage-Class',
+  CONSISTENT = 'X-Tigris-Consistent',
+  REGIONS = 'X-Tigris-Regions',
   SNAPSHOT = 'X-Tigris-Snapshot',
   SNAPSHOT_VERSION = 'X-Tigris-Snapshot-Version',
   SNAPSHOT_ENABLED = 'X-Tigris-Enable-Snapshot',
@@ -22,15 +27,21 @@ export function createTigrisClient(
   const endpoint = options?.endpoint ?? config.endpoint;
   const bucket = options?.bucket ?? config.bucket;
 
+  const skipAccessKeyCheck =
+    options?.sessionToken !== undefined &&
+    options?.organizationId !== undefined &&
+    options.sessionToken !== '' &&
+    options.organizationId !== '';
+
   if (!bucket && !skipBucketCheck) {
     return missingConfigError('bucket');
   }
 
-  if (!accessKeyId || accessKeyId === '') {
+  if (!skipAccessKeyCheck && (!accessKeyId || accessKeyId === '')) {
     return missingConfigError('accessKeyId');
   }
 
-  if (!secretAccessKey || secretAccessKey === '') {
+  if (!skipAccessKeyCheck && (!secretAccessKey || secretAccessKey === '')) {
     return missingConfigError('secretAccessKey');
   }
 
@@ -38,7 +49,11 @@ export function createTigrisClient(
     return missingConfigError('endpoint');
   }
 
-  const key = `${accessKeyId}-${secretAccessKey}-${endpoint}`;
+  let key = `${accessKeyId}-${endpoint}`;
+
+  if (options?.sessionToken && options?.organizationId) {
+    key = `${options.sessionToken}-${options.organizationId}-${endpoint}`;
+  }
 
   const cachedClient = cachedClients.get(key);
 
@@ -48,12 +63,29 @@ export function createTigrisClient(
 
   const client = new S3Client({
     credentials: {
-      accessKeyId,
-      secretAccessKey,
+      accessKeyId: accessKeyId ?? '',
+      secretAccessKey: secretAccessKey ?? '',
+      sessionToken: options?.sessionToken ?? undefined,
     },
     region: 'auto',
     endpoint: endpoint ?? 'https://t3.storage.dev',
   });
+
+  if (options?.sessionToken && options?.organizationId) {
+    client.middlewareStack.add(
+      (next) => async (args) => {
+        const req = args.request as HttpRequest;
+        req.headers[TigrisHeaders.NAMESPACE] = options.organizationId!;
+        const result = await next(args);
+        return result;
+      },
+      {
+        name: 'x-Tigris-Namespace-Middleware',
+        step: 'build',
+        override: true,
+      }
+    );
+  }
 
   if (!client) {
     return { error: new Error('Unable to create Tigris client') };
