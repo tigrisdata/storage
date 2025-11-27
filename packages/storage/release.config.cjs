65dd50558a915f34628bf5e4d6ba331c82374f41
Abdullah Ibrahim <aibrahim@tigrisdata.com>
2025-11-27 14:40:40 +0100

    feat: multi package publish setup (#30)
    * ci: multi package publish setup

---
+++ packages/storage/release.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  tagFormat: 'v${version}',
+  branches: [
+    'main',
+    {
+      name: 'next',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+};
