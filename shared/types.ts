7e3155040611607b1436d9aacdb29fbb7ba87e7a
A Ibrahim <aibrahim@tigrisdata.com>
2026-04-28 15:32:15 +0200

    refactor(shared): add TigrisConfig

--- shared/types.ts
+++ shared/types.ts
@@ -7,3 +7,24 @@ export type TigrisResponse<T, E = Error> =
       error: E;
       data?: never;
     };
+
+export type TigrisEndpointsConfig = {
+  endpoint?: string;
+  iamEndpoint?: string;
+  mgmtEndpoint?: string;
+};
+
+export type TigrisAuthConfig = {
+  accessKeyId?: string;
+  secretAccessKey?: string;
+  sessionToken?: string;
+  organizationId?: string;
+  credentialProvider?: () => Promise<{
+    accessKeyId: string;
+    secretAccessKey: string;
+    sessionToken?: string;
+    expiration?: Date;
+  }>;
+};
+
+export type TigrisConfig = TigrisEndpointsConfig & TigrisAuthConfig;
