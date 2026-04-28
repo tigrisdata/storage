e06137fdb8f6ce688d68a93f876475c8190973c6
A Ibrahim <aibrahim@tigrisdata.com>
2026-04-28 15:35:41 +0200

    fix(storage): use TigrisConfig to extend storage config

--- packages/storage/src/lib/types.ts
+++ packages/storage/src/lib/types.ts
@@ -1,19 +1,8 @@
-import type { TigrisResponse } from '@shared/types';
+import type { TigrisConfig, TigrisResponse } from '@shared/types';
 
 export type TigrisStorageConfig = {
   bucket?: string;
-  accessKeyId?: string;
-  secretAccessKey?: string;
-  endpoint?: string;
-  sessionToken?: string;
-  organizationId?: string;
   forcePathStyle?: boolean;
-  credentialProvider?: () => Promise<{
-    accessKeyId: string;
-    secretAccessKey: string;
-    sessionToken?: string;
-    expiration?: Date;
-  }>;
-};
+} & TigrisConfig;
 
 export type TigrisStorageResponse<T, E = Error> = TigrisResponse<T, E>;
